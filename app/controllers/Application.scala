c0770e0d6baba71fbf0146cf7ad0678a9ef3cf1e
retroryan <ryan@grandcloud.com>
2014-10-06 18:23:51 -0600

    adding local stuff

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,18 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-object Application extends Controller {
+object Application extends BaseController {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+
+      request =>
+          println("Languages: " + request.acceptLanguages.map(_.code).mkString(", "))
+
+          decorate(views.html.index("Hello"),
+              "Online Check-In - Norwegian Cruise Line",
+              views.html.layout.apply)
+
+           Ok(views.html.index("Your new application is ready."))
   }
 
 }
---
+++ app/controllers/BaseController.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import play.twirl.api.Html
+import play.api.mvc.Results.Status
+import play.api.mvc.{Controller, Result, RequestHeader}
+import play.api.i18n.Lang
+
+
+trait BaseController extends Controller {
+
+    def decorate(
+                    content: Html,
+                    title: String = "Norwegian Cruise Line",
+                    layout: => (String, Html) => Html,
+                    status: Status = Ok
+                    )(
+                    implicit request: RequestHeader
+                    ): Result = {
+        val locale = request.getQueryString("locale")
+
+
+        if (locale.isDefined)
+            status(layout(title, content)).withLang(Lang(locale.get))
+        else
+            status(layout(title, content))
+    }
+
+    implicit override def request2lang(implicit request: RequestHeader) = {
+        //TODO: Does not saves it in the cookie
+
+        val localeFromGET = request.getQueryString("locale")
+        val maybeLangFromCookie = request.cookies.get("PLAY_LANG").flatMap(c => Lang.get(c.value))
+        if (localeFromGET.isDefined) {
+            Lang(localeFromGET.get)
+        } else {
+            maybeLangFromCookie.getOrElse(play.api.i18n.Lang.preferred(request.acceptLanguages))
+        }
+
+        //}.getOrElse(request.acceptLanguages.headOption.getOrElse(play.api.i18n.Lang.defaultLang))
+    }
+
+}
